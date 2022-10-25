8ca61f0d828cb31133095ae8913c9ed6173b942b
Jean Egalon <egalon.jean@gmail.com>
2022-10-25 17:10:38 -0300

    feito o requisito 7

---
+++ src/models/Category.js
@@ -0,0 +1,24 @@
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    name: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+  }, {
+    timestamps: false,
+    tableName: 'categories',
+  });
+
+  Category.associate = (models) => {
+    Category.hasMany(models.PostCategory,
+     { foreignKey: 'post_id', as: 'post_categories' });
+  };
+
+  return Category;
+}
