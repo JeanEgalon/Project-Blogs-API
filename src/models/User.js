cc72bfd6d3184dbb456989ad85ff67215ac2c444
Jean Egalon <egalon.jean@gmail.com>
2022-10-25 17:01:04 -0300

    feito o segundo requisito

---
+++ src/models/User.js
@@ -0,0 +1,31 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    display_name: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    email: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    password: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    image: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+  }, {
+    timestamps: false,
+    tableName: 'users',
+  });
+
+  return User;
+}
